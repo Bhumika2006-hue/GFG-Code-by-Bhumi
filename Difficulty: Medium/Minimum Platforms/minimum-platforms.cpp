a3f18c680afd3fcf0ccdd233c36cf1f648a97c19
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-01-08 16:30:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Platforms/minimum-platforms.cpp
@@ -0,0 +1,29 @@
+class Solution {
+  public:
+    // Function to find the minimum number of platforms required at the
+    // railway station such that no train waits.
+    int minPlatform(vector<int>& arr, vector<int>& dep) {
+        // Code with Radheshyam (.^.)
+        sort(arr.begin(),arr.end());
+        sort(dep.begin(),dep.end());
+        int count=0;
+        int maxi=-1;
+        int i=0;
+        int j=0;
+        int n=arr.size();
+        
+        while(i<n && j<n){
+            if(arr[i]<=dep[j]) {
+                count++;
+                i++;
+            }
+            else{
+                count--;
+                j++;
+            }
+            maxi=max(maxi,count);
+        }
+         maxi=max(maxi,count);
+         return maxi;
+    }
+};
