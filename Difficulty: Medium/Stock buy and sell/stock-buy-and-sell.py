49d33ecf394d4e63b5f39a21f458b2821acad08e
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-01-13 21:03:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Stock buy and sell/stock-buy-and-sell.py
@@ -0,0 +1,10 @@
+class Solution:
+    # Function to find the maximum profit
+    def stockBuySell(self, arr):
+        profit = 0
+
+        for i in range(1, len(arr)):
+            if arr[i] > arr[i - 1]:
+                profit += arr[i] - arr[i - 1]
+
+        return profit
