ef4dc5a060d4fecfd85ca2b84bc909a37b8780a3
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-01-13 20:35:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    int celebrity(vector<vector<int>>& mat) {
+        int n = mat.size();
+        int top = 0, down = n - 1;
+
+        while (top < down) {
+            if (mat[top][down] == 1)
+                top++;
+            else
+                down--;
+        }
+        int j = top;
+
+        for (int i = 0; i < n; i++) {
+            if (i != j) {
+                if (mat[j][i] != 0 || mat[i][j] != 1) {
+                    return -1;
+                }
+            }
+        }
+        return j;
+    }
+};
