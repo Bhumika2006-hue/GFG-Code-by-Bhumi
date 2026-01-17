04703ba3fab7e739abd65b5a1171b402d6282cd3
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-01-17 08:53:49 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Expression contains redundant bracket or not/expression-contains-redundant-bracket-or-not.py
@@ -0,0 +1,22 @@
+class Solution():
+    def checkRedundancy(self, s):
+        stack = []
+        operators = set(['+', '-', '*', '/'])
+
+        for ch in s:
+            if ch == ')':
+                top = stack.pop()
+                has_operator = False
+
+                while top != '(':
+                    if top in operators:
+                        has_operator = True
+                    top = stack.pop()
+
+                # If no operator inside brackets → redundant
+                if not has_operator:
+                    return True
+            else:
+                stack.append(ch)
+
+        return False
