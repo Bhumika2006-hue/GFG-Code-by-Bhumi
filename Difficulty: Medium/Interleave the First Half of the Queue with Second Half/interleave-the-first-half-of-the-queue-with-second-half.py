36f4b048076636ac90a20d819db0572b38660869
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-01-30 20:08:13 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Interleave the First Half of the Queue with Second Half/interleave-the-first-half-of-the-queue-with-second-half.py
@@ -0,0 +1,19 @@
+from collections import deque
+
+class Solution:
+    def rearrangeQueue(self, q):
+        n = len(q)
+        half = n // 2
+        
+        first = deque()
+        
+        # Step 1: Move first half into another queue
+        for _ in range(half):
+            first.append(q.popleft())
+        
+        # Step 2: Interleave
+        while first:
+            q.append(first.popleft())
+            q.append(q.popleft())
+        
+        return q
