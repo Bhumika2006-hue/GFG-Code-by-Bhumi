f3f6f9165d28d54209a79cebcdc045a56ecdcece
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-02-05 21:40:36 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Sorted Array Search/sorted-array-search.py
@@ -0,0 +1,15 @@
+class Solution:
+    def searchInSorted(self, arr, k):
+        left, right = 0, len(arr) - 1
+        
+        while left <= right:
+            mid = (left + right) // 2
+            
+            if arr[mid] == k:
+                return True
+            elif arr[mid] < k:
+                left = mid + 1
+            else:
+                right = mid - 1
+        
+        return False
