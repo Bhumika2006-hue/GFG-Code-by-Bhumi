1b04c475651822b154eca74d3891235cb11e62bc
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-02-05 21:39:32 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/N meetings in one room/n-meetings-in-one-room.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximumMeetings(self, start, end):
+        meetings = sorted(zip(start, end), key=lambda x: x[1])
+        
+        count = 0
+        last_end = -1
+        
+        for s, e in meetings:
+            if s > last_end:
+                count += 1
+                last_end = e
+        
+        return count
