fdc80ba9db2e31c27c3ab8e1bf74e3cf5ec717a4
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-04-17 23:29:26 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Anagram Palindrome/anagram-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+    bool canFormPalindrome(string &s) {
+        vector<int>freq(26,0);
+        for(char ch:s)
+        {
+            freq[(ch-'a')]++;
+        }
+        int odd=0;
+        for(auto it:freq)
+        {
+            if(it%2!=0)
+            {
+                odd++;
+            }
+            if(odd>1)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
