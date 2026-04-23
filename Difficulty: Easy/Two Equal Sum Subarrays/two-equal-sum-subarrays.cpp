36c5c2180afd98ae9bf9542479dea442168ecdc4
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-04-23 18:42:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Two Equal Sum Subarrays/two-equal-sum-subarrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+  public:
+    bool canSplit(vector<int>& arr) {
+        int arrSum=0,subArrSum=0,n=arr.size();
+        //poore array ka sum
+        for(int i=0;i<n;i++){
+            arrSum+=arr[i];
+        }
+        //sub array ka sum *2== arrSum 
+        for(int i=0;i<n;i++){
+            subArrSum+=arr[i];
+            if(subArrSum*2==arrSum) return true;
+        }
+        return false;
+    }
+};
