24d28813a8955798d4246415ce5b5790295c9e5d
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-04-26 09:18:37 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.py
+++ Difficulty: Easy/Common in 3 Sorted Arrays/common-in-3-sorted-arrays.py
@@ -1,25 +1,26 @@
 class Solution:
-    def commonElements(self, arr1, arr2, arr3):
-        i = j = k = 0
-        res = []
-
-        n1, n2, n3 = len(arr1), len(arr2), len(arr3)
-
-        while i < n1 and j < n2 and k < n3:
-            if arr1[i] == arr2[j] == arr3[k]:
+    def commonElements(self, a, b, c):
+        i, j, k = 0, 0, 0
+        result = []
+        
+        while i < len(a) and j < len(b) and k < len(c):
+            
+            # If all elements are equal
+            if a[i] == b[j] == c[k]:
                 # Avoid duplicates
-                if not res or res[-1] != arr1[i]:
-                    res.append(arr1[i])
+                if not result or result[-1] != a[i]:
+                    result.append(a[i])
+                
                 i += 1
                 j += 1
                 k += 1
+            
+            # Move the pointer with smallest value
+            elif a[i] < b[j]:
+                i += 1
+            elif b[j] < c[k]:
+                j += 1
             else:
-                mn = min(arr1[i], arr2[j], arr3[k])
-                if arr1[i] == mn:
-                    i += 1
-                elif arr2[j] == mn:
-                    j += 1
-                else:
-                    k += 1
-
-        return res if res else [-1]
+                k += 1
+        
+        return result
