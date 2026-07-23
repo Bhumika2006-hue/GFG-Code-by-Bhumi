63a7c61d2de9a017943f680c15805d4338ea3b7e
Bhumika Tewari <bhumi.tewari06@gmail.com>
2026-07-23 22:39:48 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Preorder Traversal/preorder-traversal.cpp
@@ -0,0 +1,22 @@
+class Solution {
+  private:
+    void traverse(Node* root, vector<int>& result) {
+        if (root == NULL) return;
+        
+        // 1. Visit Root
+        result.push_back(root->data);
+        
+        // 2. Visit Left Subtree
+        traverse(root->left, result);
+        
+        // 3. Visit Right Subtree
+        traverse(root->right, result);
+    }
+
+  public:
+    vector<int> preOrder(Node* root) {
+        vector<int> result;
+        traverse(root, result);
+        return result;
+    }
+};
